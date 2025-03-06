25c5710fffedfdbeb002a5e013a1381a0529b74a
NiloCK <colin@mrkennedy.ca>
2025-03-06 14:41:27 -0400

    layout updates

---
+++ src/pages/Blog.vue
@@ -0,0 +1,28 @@
+<template>
+  <Layout>
+    <div>
+      <div v-for="post in $page.posts.edges" :key="post.node.id">
+        <h3>
+          {{ post.node.date.split("T")[0] }}:
+          <g-link :to="post.node.path">{{ post.node.title }}</g-link>
+        </h3>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query {
+  posts: allBlogPost {
+    edges {
+      node {
+        id
+        title
+        date
+        path
+        content
+      }
+    }
+  }
+}
+</page-query>
---
+++ src/templates/BlogPost.vue
@@ -0,0 +1,25 @@
+<template>
+  <Layout>
+    <h1>{{ $page.blogPost.title }}</h1>
+    <div v-html="$page.blogPost.content" />
+  </Layout>
+</template>
+
+<page-query>
+query BlogPost ($id: ID!) {
+  blogPost(id: $id) {
+    title
+    content
+  }
+}
+</page-query>
+
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: this.$page.blogPost.title,
+    };
+  },
+};
+</script>
