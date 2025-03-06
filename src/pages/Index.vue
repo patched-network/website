c2011d63ee95a4f1b2a389b340c271d4c6b9ebe1
NiloCK <colin@mrkennedy.ca>
2025-03-06 14:24:52 -0400

    index -> currentPost

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,40 @@
 <template>
   <Layout>
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+    <div>
+      <div v-if="latestPost">
+        <h1>{{ latestPost.title }}</h1>
+        <div v-html="latestPost.content"></div>
+      </div>
+      <p v-else>No posts found.</p>
+    </div>
   </Layout>
 </template>
 
-<script>
-export default {
-  metaInfo: {
-    title: 'Hello, world!'
+<page-query>
+query {
+  posts: allBlogPost(sortBy: "date", order: DESC, limit: 1) {
+    edges {
+      node {
+        id
+        title
+        path
+        content
+        date
+      }
+    }
   }
 }
-</script>
+</page-query>
 
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
-</style>
+<script>
+export default {
+  computed: {
+    latestPost() {
+      if (this.$page.posts.edges.length > 0) {
+        return this.$page.posts.edges[0].node;
+      }
+      return null;
+    },
+  },
+};
+</script>
