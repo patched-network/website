abc4111b698bea7da2e7b1bbcf8441176d0190b9
NiloCK <colin@mrkennedy.ca>
2025-03-29 14:49:20 -0300

    add rss feed

--- gridsome.config.js
+++ gridsome.config.js
@@ -7,6 +7,7 @@
 
 module.exports = {
   siteName: "patched.network",
+  siteUrl: "https://patched.network", // Add your site URL here (required for RSS feed)
   plugins: [
     {
       use: "@gridsome/source-filesystem",
@@ -21,6 +22,28 @@ module.exports = {
           options: {
             field: "title",
           },
+        },
+      },
+    },
+    {
+      use: "gridsome-plugin-rss",
+      options: {
+        contentTypeName: "BlogPost",
+        latest: true,
+        feedOptions: {
+          title: "patched.network Blog",
+          feed_url: "https://patched.network/rss.xml",
+          site_url: "https://patched.network",
+        },
+        feedItemOptions: (node) => ({
+          title: node.title,
+          description: node.content, // You may want to use a summary field if available
+          url: `https://patched.network${node.path}`,
+          date: node.date,
+        }),
+        output: {
+          dir: "./static",
+          name: "rss.xml",
         },
       },
     },
